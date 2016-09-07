8520a4ab85d9bd5539c019fc42f7266864d033cc
unknown <alumno@LAB02PC07.labs.extension>
2016-09-07 09:49:02 -0300

    Reorganizacion del codigo, implementacion de region y lista de autos.

--- claseTres/claseTres/carrera.cs
+++ claseTres/claseTres/carrera.cs
@@ -17,10 +17,7 @@ namespace claseTres
         //auto autoCinco;
         //auto autoSeis;
 
-        public List<auto> ListaAutos;
-
-        private Random randomKilometro;
-        private Random randomTiempo;
+        #region constructores
 
         public carrera()
         {
@@ -46,6 +43,34 @@ namespace claseTres
 
         }
 
+        #endregion
+
+        #region Metodos
+
+        private bool AgregarAuto(auto unAuto)
+        {
+            this.ListaAutos.Add(unAuto);
+
+            return true;
+        }
+
+        public static carrera operator +(carrera carrera, auto unAuto)
+        {
+            carrera.AgregarAuto(unAuto);
+
+            return carrera;
+        }
+
+        #endregion
+
+
+        public List<auto> ListaAutos;
+
+        private Random randomKilometro;
+        private Random randomTiempo;
+
+        
+
         /// <summary>
         /// Este metodo muestra los competidores
         /// </summary>
@@ -56,12 +81,7 @@ namespace claseTres
             {
                 item.MostrarAuto();
             }
-            //this.autoUno.MostrarAuto();
-            //this.autoDos.MostrarAuto();
-            //this.autoTres.MostrarAuto();
-            //this.autoCuatro.MostrarAuto();
-            //this.autoCinco.MostrarAuto();
-            //this.autoSeis.MostrarAuto();
+            
         }
 
 
@@ -94,115 +114,7 @@ namespace claseTres
         /// </summary>
         private void Ganador()
         {
-            //if (this.autoUno.DevolverKilometro() > this.autoDos.DevolverKilometro())
-            //{
-            //    if (this.autoUno.DevolverKilometro() > this.autoTres.DevolverKilometro())
-            //    {
-            //        if (this.autoUno.DevolverKilometro() > this.autoCuatro.DevolverKilometro())
-            //        {
-            //            if (this.autoUno.DevolverKilometro() > this.autoCinco.DevolverKilometro())
-            //            {
-            //                if (this.autoUno.DevolverKilometro() > this.autoSeis.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 1");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
-
-            //if (this.autoDos.DevolverKilometro() > this.autoUno.DevolverKilometro())
-            //{
-            //    if (this.autoDos.DevolverKilometro() > this.autoTres.DevolverKilometro())
-            //    {
-            //        if (this.autoDos.DevolverKilometro() > this.autoCuatro.DevolverKilometro())
-            //        {
-            //            if (this.autoDos.DevolverKilometro() > this.autoCinco.DevolverKilometro())
-            //            {
-            //                if (this.autoDos.DevolverKilometro() > this.autoSeis.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 2");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
-            //if (this.autoTres.DevolverKilometro() > this.autoUno.DevolverKilometro())
-            //{
-            //    if (this.autoTres.DevolverKilometro() > this.autoDos.DevolverKilometro())
-            //    {
-            //        if (this.autoTres.DevolverKilometro() > this.autoCuatro.DevolverKilometro())
-            //        {
-            //            if (this.autoTres.DevolverKilometro() > this.autoCinco.DevolverKilometro())
-            //            {
-            //                if (this.autoTres.DevolverKilometro() > this.autoSeis.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 3");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
-            //if (this.autoCuatro.DevolverKilometro() > this.autoUno.DevolverKilometro())
-            //{
-            //    if (this.autoCuatro.DevolverKilometro() > this.autoDos.DevolverKilometro())
-            //    {
-            //        if (this.autoCuatro.DevolverKilometro() > this.autoTres.DevolverKilometro())
-            //        {
-            //            if (this.autoCuatro.DevolverKilometro() > this.autoCinco.DevolverKilometro())
-            //            {
-            //                if (this.autoCuatro.DevolverKilometro() > this.autoSeis.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 4");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
-            //if (this.autoCinco.DevolverKilometro() > this.autoUno.DevolverKilometro())
-            //{
-            //    if (this.autoCinco.DevolverKilometro() > this.autoDos.DevolverKilometro())
-            //    {
-            //        if (this.autoCinco.DevolverKilometro() > this.autoTres.DevolverKilometro())
-            //        {
-            //            if (this.autoCinco.DevolverKilometro() > this.autoCuatro.DevolverKilometro())
-            //            {
-            //                if (this.autoCinco.DevolverKilometro() > this.autoSeis.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 5");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
-            //if (this.autoSeis.DevolverKilometro() > this.autoUno.DevolverKilometro())
-            //{
-            //    if (this.autoSeis.DevolverKilometro() > this.autoDos.DevolverKilometro())
-            //    {
-            //        if (this.autoSeis.DevolverKilometro() > this.autoTres.DevolverKilometro())
-            //        {
-            //            if (this.autoSeis.DevolverKilometro() > this.autoCuatro.DevolverKilometro())
-            //            {
-            //                if (this.autoSeis.DevolverKilometro() > this.autoCinco.DevolverKilometro())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 6");
-            //                }
-            //            }
-            //        }
-
-            //    }
-            //}
-
+            
         }// Fin metodo Ganador()
 
 
@@ -211,13 +123,7 @@ namespace claseTres
         /// </summary>
         private void MostrarKilometros()
         {
-            //Console.WriteLine("\nKilometros recorridos por competidor\n");
-            //Console.WriteLine("auto 1: " + this.autoUno.DevolverKilometro());
-            //Console.WriteLine("auto 2: " + this.autoDos.DevolverKilometro());
-            //Console.WriteLine("auto 3: " + this.autoTres.DevolverKilometro());
-            //Console.WriteLine("auto 4: " + this.autoCuatro.DevolverKilometro());
-            //Console.WriteLine("auto 5: " + this.autoCinco.DevolverKilometro());
-            //Console.WriteLine("auto 6: " + this.autoSeis.DevolverKilometro());
+           
         }
 
 
@@ -232,12 +138,7 @@ namespace claseTres
         {
             for (int i = 0; i < (int)distancia; i++)
             {
-                //this.autoUno.AgregarTiempo(randomTiempo.Next(10, 100));
-                //this.autoDos.AgregarTiempo(randomTiempo.Next(10, 100));
-                //this.autoTres.AgregarTiempo(randomTiempo.Next(10, 100));
-                //this.autoCuatro.AgregarTiempo(randomTiempo.Next(10, 100));
-                //this.autoCinco.AgregarTiempo(randomTiempo.Next(10, 100));
-                //this.autoSeis.AgregarTiempo(randomTiempo.Next(10, 100));
+                
             }
 
             this.GanadorTiempo();
@@ -250,111 +151,7 @@ namespace claseTres
         /// </summary>
         private void GanadorTiempo()
         {
-            //if (this.autoUno.DevolverTiempo() < this.autoDos.DevolverTiempo())
-            //{
-            //    if (this.autoUno.DevolverTiempo() < this.autoTres.DevolverTiempo())
-            //    {
-            //        if (this.autoUno.DevolverTiempo() < this.autoCuatro.DevolverTiempo())
-            //        {
-            //            if (this.autoUno.DevolverTiempo() < this.autoCinco.DevolverTiempo())
-            //            {
-            //                if (this.autoUno.DevolverTiempo() < this.autoSeis.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 1");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-
-            //if (this.autoDos.DevolverTiempo() < this.autoUno.DevolverTiempo())
-            //{
-            //    if (this.autoDos.DevolverTiempo() < this.autoTres.DevolverTiempo())
-            //    {
-            //        if (this.autoDos.DevolverTiempo() < this.autoCuatro.DevolverTiempo())
-            //        {
-            //            if (this.autoDos.DevolverTiempo() < this.autoCinco.DevolverTiempo())
-            //            {
-            //                if (this.autoDos.DevolverTiempo() < this.autoSeis.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 2");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-
-            //if (this.autoTres.DevolverTiempo() < this.autoUno.DevolverTiempo())
-            //{
-            //    if (this.autoTres.DevolverTiempo() < this.autoDos.DevolverTiempo())
-            //    {
-            //        if (this.autoTres.DevolverTiempo() < this.autoCuatro.DevolverTiempo())
-            //        {
-            //            if (this.autoTres.DevolverTiempo() < this.autoCinco.DevolverTiempo())
-            //            {
-            //                if (this.autoTres.DevolverTiempo() < this.autoSeis.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 3");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-
-            //if (this.autoCuatro.DevolverTiempo() < this.autoUno.DevolverTiempo())
-            //{
-            //    if (this.autoCuatro.DevolverTiempo() < this.autoDos.DevolverTiempo())
-            //    {
-            //        if (this.autoCuatro.DevolverTiempo() < this.autoTres.DevolverTiempo())
-            //        {
-            //            if (this.autoCuatro.DevolverTiempo() < this.autoCinco.DevolverTiempo())
-            //            {
-            //                if (this.autoCuatro.DevolverTiempo() < this.autoSeis.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 4");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-            //if (this.autoCinco.DevolverTiempo() < this.autoUno.DevolverTiempo())
-            //{
-            //    if (this.autoCinco.DevolverTiempo() < this.autoDos.DevolverTiempo())
-            //    {
-            //        if (this.autoCinco.DevolverTiempo() < this.autoTres.DevolverTiempo())
-            //        {
-            //            if (this.autoCinco.DevolverTiempo() < this.autoCuatro.DevolverTiempo())
-            //            {
-            //                if (this.autoCinco.DevolverTiempo() < this.autoSeis.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 5");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-            //if (this.autoSeis.DevolverTiempo() < this.autoUno.DevolverTiempo())
-            //{
-            //    if (this.autoSeis.DevolverTiempo() < this.autoDos.DevolverTiempo())
-            //    {
-            //        if (this.autoSeis.DevolverTiempo() < this.autoTres.DevolverTiempo())
-            //        {
-            //            if (this.autoSeis.DevolverTiempo() < this.autoCuatro.DevolverTiempo())
-            //            {
-            //                if (this.autoSeis.DevolverTiempo() < this.autoCinco.DevolverTiempo())
-            //                {
-            //                    Console.WriteLine("\nEl ganador es el auto numero 6");
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
+            
 
 
         }//Fin metodo GanadorTiempo
@@ -364,13 +161,7 @@ namespace claseTres
         /// </summary>
         private void MostrarTiempo()
         {
-            //Console.WriteLine("\nCantidad de tiempo por competidor\n");
-            //Console.WriteLine("Minutos auto 1: {0}",this.autoUno.DevolverTiempo());
-            //Console.WriteLine("Minutos auto 3: {0}", this.autoDos.DevolverTiempo());
-            //Console.WriteLine("Minutos auto 4: {0}", this.autoTres.DevolverTiempo());
-            //Console.WriteLine("Minutos auto 5: {0}", this.autoCuatro.DevolverTiempo());
-            //Console.WriteLine("Minutos auto 6: {0}", this.autoCinco.DevolverTiempo());
-            //Console.WriteLine("Minutos auto 1: {0}", this.autoSeis.DevolverTiempo());
+            
         }
 
 
